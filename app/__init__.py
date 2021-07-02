c921ecd43f555c3a61b7d8870059ecb0799da4ae
Halbard <HalbardHobby@users.noreply.github.com>
2021-07-02 16:48:22 -0500

    🏗 Added basic scaffolding

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 import os
 
 from flask import Flask
+from . import books
 
 def create_app():
     app = Flask(__name__)
+
+    app.register_blueprint(books.bp)
 
     @app.route("/")
     def hello_world():
---
+++ app/books.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+bp = Blueprint('books', __name__, url_prefix=None)
+
+def list_boooks():
+    pass
+
+def create_book():
+    pass
+
+def read_book():
+    pass
+
+def update_book():
+    pass
+
+def delete_book():
+    pass
