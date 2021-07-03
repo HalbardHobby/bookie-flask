b5a2a7fe71aa25211959b781c303e431d6ddb286
Halbard <HalbardHobby@users.noreply.github.com>
2021-07-02 21:00:13 -0500

    🤪 Sanity check

--- app/books.py
+++ app/books.py
@@ -1,24 +1,25 @@
 from flask import Blueprint
+from .db import db, Book
 
 bp = Blueprint('books', __name__, url_prefix=None)
 
 @bp.route('/', methods=['GET'])
 @bp.route('/index', methods=['GET'])
 def list_boooks():
-    return "list books"
+    return Book.query.all()[0].isbn
 
-@bp.route('/<book_id>', methods=['POST'])
+@bp.route('/', methods=['POST'])
 def create_book(book_id):
     return "create book"
 
 @bp.route('/<book_id>', methods=['GET'])
 def read_book(book_id):
-    return "book info"
+    return "book info" + book_id
 
 @bp.route('/<book_id>', methods=['PUT'])
 def update_book(book_id):
-    return "book updated"
+    return "book updated" + book_id
 
 @bp.route('/<book_id>', methods=['DELETE'])
 def delete_book(book_id):
-    return "book deleted"
+    return "book deleted" + book_id
